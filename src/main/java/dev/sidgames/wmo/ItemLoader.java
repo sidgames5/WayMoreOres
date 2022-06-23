56ce9e051343e015ab81d06fba9acf80565aad2d
BasicCorruption <saytleader@outlook.com>
2022-06-23 18:01:00 -0400

    we starting again

---
+++ src/main/java/dev/sidgames/wmo/ItemLoader.java
@@ -0,0 +1,7 @@
+package dev.sidgames.wmo;
+
+public class ItemLoader {
+    public static void registerItems() {
+        // Register items here
+    }
+}
---
+++ src/main/java/dev/sidgames/wmo/Reference.java
@@ -0,0 +1,10 @@
+package dev.sidgames.wmo;
+
+public class Reference {
+    public static final String MOD_ID = "wmo";
+    public static final String MOD_NAME = "Way More Ores";
+    public static final String MOD_VERSION = "1.0.0";
+    public static final String[] MOD_AUTHOR = {"SidGames5", "LeWolfYT"};
+
+    public static final String MINECRAFT_VERSION = "1.18.2";
+}
