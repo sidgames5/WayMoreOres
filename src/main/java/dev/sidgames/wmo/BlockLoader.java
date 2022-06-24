2ca904e2cdf341be88276ad0ffe161180b248dcd
BasicCorruption <saytleader@outlook.com>
2022-06-23 21:10:51 -0400

    add everything

--- src/main/java/dev/sidgames/wmo/BlockLoader.java
+++ src/main/java/dev/sidgames/wmo/BlockLoader.java
@@ -11,86 +11,69 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockLoader {
+        public static Block ALUMINUM_BLOCK;
+        public static Block BAUXITE_ORE;
+        public static Block COPPER_BLOCK;
+        public static Block COPPER_ORE;
+        public static Block LITHIUM_BLOCK;
+        public static Block LITHIUM_ORE;
+        public static Block PLATINUM_BLOCK;
+        public static Block PLATINUM_ORE;
+        public static Block RUBY_BLOCK;
+        public static Block RUBY_ORE;
+        public static Block SILVER_BLOCK;
+        public static Block SILVER_ORE;
+        public static Block TIN_BLOCK;
+        public static Block TIN_ORE;
+        public static Block SAPPHIRE_BLOCK;
+        public static Block SAPPHIRE_ORE;
+        public static Block URANIUM_BLOCK;
+        public static Block URANIUM_ORE;
+
         public static void registerBlocks() {
                 // Register blocks here
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "aluminum_block"),
-                                new AluminumBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "bauxite_ore"),
-                                new BauxiteOre());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "copper_block"),
-                                new CopperBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "copper_ore"),
-                                new CopperOre());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "lithium_block"),
-                                new LithiumBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "lithium_ore"),
-                                new LithiumOre());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "platinum_block"),
-                                new PlatinumBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "platinum_ore"),
-                                new PlatinumOre());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "ruby_block"),
-                                new RubyBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "ruby_ore"),
-                                new RubyOre());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "sapphire_block"),
-                                new SapphireBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "sapphire_ore"),
-                                new SapphireOre());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "silver_block"),
-                                new SilverBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "silver_ore"),
-                                new SilverOre());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "tin_block"),
-                                new TinBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "tin_ore"),
-                                new TinOre());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "uranium_block"),
-                                new UraniumBlock());
-                Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "uranium_ore"),
-                                new UraniumOre());
+                ALUMINUM_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "aluminum_block"), new AluminumBlock());
+                BAUXITE_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "bauxite_ore"), new BauxiteOre());
+                COPPER_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "copper_block"), new CopperBlock());
+                COPPER_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "copper_ore"), new CopperOre());
+                LITHIUM_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "lithium_block"), new LithiumBlock());
+                LITHIUM_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "lithium_ore"), new LithiumOre());
+                PLATINUM_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "platinum_block"), new PlatinumBlock());
+                PLATINUM_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "platinum_ore"), new PlatinumOre());
+                RUBY_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "ruby_block"), new RubyBlock());
+                RUBY_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "ruby_ore"), new RubyOre());
+                SAPPHIRE_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "sapphire_block"), new SapphireBlock());
+                SAPPHIRE_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "sapphire_ore"), new SapphireOre());
+                SILVER_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "silver_block"), new SilverBlock());
+                SILVER_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "silver_ore"), new SilverOre());
+                TIN_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "tin_block"), new TinBlock());
+                TIN_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "tin_ore"), new TinOre());
+                URANIUM_BLOCK = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "uranium_block"), new UraniumBlock());
+                URANIUM_ORE = Registry.register(Registry.BLOCK, new Identifier(Reference.MOD_ID, "uranium_ore"), new UraniumOre());
 
                 registerBlockItems();
         }
 
         public static void registerBlockItems() {
                 // Register block items here
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "aluminum_block"),
-                                new BlockItem(new AluminumBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "bauxite_ore"),
-                                new BlockItem(new BauxiteOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "copper_block"),
-                                new BlockItem(new CopperBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "copper_ore"),
-                                new BlockItem(new CopperOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "lithium_block"),
-                                new BlockItem(new LithiumBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "lithium_ore"),
-                                new BlockItem(new LithiumOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "platinum_block"),
-                                new BlockItem(new PlatinumBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "platinum_ore"),
-                                new BlockItem(new PlatinumOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "ruby_block"),
-                                new BlockItem(new RubyBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "ruby_ore"),
-                                new BlockItem(new RubyOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "sapphire_block"),
-                                new BlockItem(new SapphireBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "sapphire_ore"),
-                                new BlockItem(new SapphireOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "silver_block"),
-                                new BlockItem(new SilverBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "silver_ore"),
-                                new BlockItem(new SilverOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "tin_block"),
-                                new BlockItem(new TinBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "tin_ore"),
-                                new BlockItem(new TinOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "uranium_block"),
-                                new BlockItem(new UraniumBlock(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "uranium_ore"),
-                                new BlockItem(new UraniumOre(), new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "aluminum_block"), new BlockItem(ALUMINUM_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "bauxite_ore"), new BlockItem(BAUXITE_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "copper_block"), new BlockItem(COPPER_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "copper_ore"), new BlockItem(COPPER_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "lithium_block"), new BlockItem(LITHIUM_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "lithium_ore"), new BlockItem(LITHIUM_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "platinum_block"), new BlockItem(PLATINUM_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "platinum_ore"), new BlockItem(PLATINUM_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "ruby_block"), new BlockItem(RUBY_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "ruby_ore"), new BlockItem(RUBY_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "sapphire_block"), new BlockItem(SAPPHIRE_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "sapphire_ore"), new BlockItem(SAPPHIRE_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "silver_block"), new BlockItem(SILVER_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "silver_ore"), new BlockItem(SILVER_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "tin_block"), new BlockItem(TIN_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "tin_ore"), new BlockItem(TIN_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "uranium_block"), new BlockItem(URANIUM_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+                Registry.register(Registry.ITEM, new Identifier(Reference.MOD_ID, "uranium_ore"), new BlockItem(URANIUM_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 
         }
 }
