8768136fbe5d9d6e6a99d009d6c66a8b113104fd
BasicCorruption <saytleader@outlook.com>
2022-06-24 13:22:45 -0400

    Update LeadIngot.java

--- src/main/java/dev/sidgames/wmo/item/LeadIngot.java
+++ src/main/java/dev/sidgames/wmo/item/LeadIngot.java
@@ -1,11 +1,7 @@
 package dev.sidgames.wmo.item;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+import dev.sidgames.wmo.effects.LeadPoisoning;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -21,12 +17,7 @@ public class LeadIngot extends ItemBase {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         // give the player the poison effect
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 20 * 60 * 5));
+        LeadPoisoning.trigger(world, user, hand);
         return super.use(world, user, hand);
     }
 }
