412d7f3b956658d78cd155d71a1d4858de340848
BasicCorruption <saytleader@outlook.com>
2022-06-24 19:05:01 -0400

    Update Reference.java

--- src/main/java/dev/sidgames/wmo/Reference.java
+++ src/main/java/dev/sidgames/wmo/Reference.java
@@ -1,6 +1,10 @@
 package dev.sidgames.wmo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Reference {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Reference.MOD_ID);
     public static final String MOD_ID = "wmo";
     public static final String MOD_NAME = "Way More Ores";
     public static final String MOD_VERSION = "1.0.0";
