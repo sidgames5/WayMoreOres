3c227505848f54cf322b838342585a11064443ac
BasicCorruption <saytleader@outlook.com>
2022-06-24 19:05:06 -0400

    Update WayMoreOres.java

--- src/main/java/dev/sidgames/wmo/WayMoreOres.java
+++ src/main/java/dev/sidgames/wmo/WayMoreOres.java
@@ -6,14 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WayMoreOres implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(Reference.MOD_ID);
-
 	@Override
 	public void onInitialize() {
-
 		ItemLoader.registerItems();
 		BlockLoader.registerBlocks();
 		WMORegistries.registerModStuffs();
