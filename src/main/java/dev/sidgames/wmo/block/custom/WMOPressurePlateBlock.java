25f9002f43c8a16b6e5fdeaf1ff025aa4d864740
BasicCorruption <saytleader@outlook.com>
2022-06-24 15:26:08 -0400

    yes we are adding pressure plates and buttons

---
+++ src/main/java/dev/sidgames/wmo/block/custom/WMOPressurePlateBlock.java
@@ -0,0 +1,9 @@
+package dev.sidgames.wmo.block.custom;
+
+import net.minecraft.block.PressurePlateBlock;
+
+public class WMOPressurePlateBlock extends PressurePlateBlock {
+    public WMOPressurePlateBlock(ActivationRule type, Settings settings) {
+        super(type, settings);
+    }
+}
---
+++ src/main/java/dev/sidgames/wmo/block/custom/WMOStoneButtonBlock.java
@@ -0,0 +1,10 @@
+package dev.sidgames.wmo.block.custom;
+
+import net.minecraft.block.StoneButtonBlock;
+
+public class WMOStoneButtonBlock extends StoneButtonBlock {
+    public WMOStoneButtonBlock(Settings settings) {
+        super(settings);
+    }
+
+}
