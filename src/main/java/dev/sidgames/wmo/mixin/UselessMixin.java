d6e88ee1a418b006cbdecd105aa5236fc08f941e
BasicCorruption <saytleader@outlook.com>
2022-06-24 14:40:56 -0400

    mixin preparation?

---
+++ src/main/java/dev/sidgames/wmo/mixin/UselessMixin.java
@@ -0,0 +1,16 @@
+package dev.sidgames.wmo.mixin;
+
+import dev.sidgames.wmo.WayMoreOres;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class UselessMixin {
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        WayMoreOres.LOGGER.info("This line is printed by an example mod mixin!");
+    }
+}
