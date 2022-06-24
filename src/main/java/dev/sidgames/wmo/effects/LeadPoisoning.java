22feb23edfd7b59a2ec9d3df66021635a32ac2cf
BasicCorruption <saytleader@outlook.com>
2022-06-24 13:26:38 -0400

    Update LeadPoisoning.java

--- src/main/java/dev/sidgames/wmo/effects/LeadPoisoning.java
+++ src/main/java/dev/sidgames/wmo/effects/LeadPoisoning.java
@@ -10,12 +10,12 @@ import net.minecraft.world.World;
 
 public class LeadPoisoning {
     public static TypedActionResult<ItemStack> trigger(World world, PlayerEntity user, Hand hand) {
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 20 * 60 * 5));
-        user.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 20 * 60 * 5));
+        user.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 10 * 60 * 5));
+        user.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 10 * 60 * 5));
+        user.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 10 * 60 * 5));
+        user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 10 * 60 * 5));
+        user.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 10 * 60 * 5));
+        user.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 10 * 60 * 5));
         return null;
     }
 }
