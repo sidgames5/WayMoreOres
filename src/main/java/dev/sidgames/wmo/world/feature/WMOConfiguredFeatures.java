771f0aba045292c81aa519acfa1e97f7c800e6c6
BasicCorruption <saytleader@outlook.com>
2022-06-25 17:45:17 -0400

    Update WMOConfiguredFeatures.java

--- src/main/java/dev/sidgames/wmo/world/feature/WMOConfiguredFeatures.java
+++ src/main/java/dev/sidgames/wmo/world/feature/WMOConfiguredFeatures.java
@@ -8,8 +8,16 @@ import net.minecraft.world.gen.feature.*;
 import java.util.List;
 
 public class WMOConfiguredFeatures {
-    public static final List<OreFeatureConfig.Target> OVERWORLD_BAUXITE_ORES = List.of(OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, BlockLoader.BAUXITE_ORE.getDefaultState()));
-    public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> BAUXITE_ORE = ConfiguredFeatures.register("bauxite_ore", Feature.ORE, new OreFeatureConfig(OVERWORLD_BAUXITE_ORES, 9));
+    public static final List<OreFeatureConfig.Target> OVERWORLD_BAUXITE_ORES = List.of(
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, BlockLoader.BAUXITE_ORE.getDefaultState())
+            //OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockLoader.DEEPSLATE_BAUXITE_ORE.getDefaultState())
+    );
+//    public static final List<OreFeatureConfig.Target> OVERWORLD_LITHIUM_ORES = List.of(
+//            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, BlockLoader.LITHIUM_ORE.getDefaultState()),
+//            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockLoader.DEEPSLATE_LITHIUM_ORE.getDefaultState())
+//    );
+    public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> BAUXITE_ORE = ConfiguredFeatures.register("bauxite_ore", Feature.ORE, new OreFeatureConfig(OVERWORLD_BAUXITE_ORES, 2));
+    //public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> LITHIUM_ORE = ConfiguredFeatures.register("lithium_ore", Feature.ORE, new OreFeatureConfig(OVERWORLD_LITHIUM_ORES, 9));
 
     public static void registerConfiguredFeatures() {
         Reference.LOGGER.info("Registering configured features");
