78585d36ced625b7e0de56e18e2371916cae74ea
BasicCorruption <saytleader@outlook.com>
2022-06-25 17:45:42 -0400

    Create WMOOreGeneration.java

---
+++ src/main/java/dev/sidgames/wmo/world/gen/WMOOreGeneration.java
@@ -0,0 +1,13 @@
+package dev.sidgames.wmo.world.gen;
+
+import dev.sidgames.wmo.world.feature.WMOPlacedFeatures;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.gen.GenerationStep;
+
+public class WMOOreGeneration {
+    public static void generateOres() {
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, WMOPlacedFeatures.BAUXITE_ORE_PLACED.getKey().get());
+        //BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, WMOPlacedFeatures.LITHIUM_ORE_PLACED.getKey().get());
+    }
+}
