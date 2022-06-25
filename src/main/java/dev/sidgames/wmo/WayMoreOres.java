409bc60295155cabfac72ade75804f99835886e2
BasicCorruption <saytleader@outlook.com>
2022-06-25 17:45:11 -0400

    Update WayMoreOres.java

--- src/main/java/dev/sidgames/wmo/WayMoreOres.java
+++ src/main/java/dev/sidgames/wmo/WayMoreOres.java
@@ -1,9 +1,9 @@
 package dev.sidgames.wmo;
 
 import dev.sidgames.wmo.util.WMORegistries;
+import dev.sidgames.wmo.world.feature.WMOConfiguredFeatures;
+import dev.sidgames.wmo.world.gen.WMOWorldGen;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class WayMoreOres implements ModInitializer {
 	@Override
@@ -11,5 +11,7 @@ public class WayMoreOres implements ModInitializer {
 		ItemLoader.registerItems();
 		BlockLoader.registerBlocks();
 		WMORegistries.registerModStuffs();
+		WMOConfiguredFeatures.registerConfiguredFeatures();
+		WMOWorldGen.generateModWorldGen();
 	}
 }
