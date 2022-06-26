ed7bbefcde83ce4e9498d3dc9147f4df7a443776
BasicCorruption <saytleader@outlook.com>
2022-06-26 18:42:47 -0400

    Create LeadIngot.java

---
+++ src/main/java/dev/sidgames/wmo/item/custom/LeadIngot.java
@@ -0,0 +1,39 @@
+package dev.sidgames.wmo.item.custom;
+
+import dev.sidgames.wmo.effects.LeadPoisoning;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class LeadIngot extends Item {
+    public LeadIngot(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(new TranslatableText("item.wmo.lead_ingot.tooltip.shift"));
+        } else {
+            tooltip.add(new TranslatableText("item.wmo.lead_ingot.tooltip.hint"));
+        }
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        // give the player the poison effect
+        LeadPoisoning.trigger(world, user, hand);
+        return super.use(world, user, hand);
+    }
+}
+
