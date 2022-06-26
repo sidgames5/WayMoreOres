e92374f4f6b41fd03ed0e7de69bc846ccff60118
BasicCorruption <saytleader@outlook.com>
2022-06-26 18:45:23 -0400

    Create WMOStoneButtonBlock.java

---
+++ src/main/java/dev/sidgames/wmo/block/functional/WMOStoneButtonBlock.java
@@ -0,0 +1,10 @@
+package dev.sidgames.wmo.block.functional;
+
+import net.minecraft.block.StoneButtonBlock;
+
+public class WMOStoneButtonBlock extends StoneButtonBlock {
+    public WMOStoneButtonBlock(Settings settings) {
+        super(settings);
+    }
+
+}
