eddd781f477ab46fc9d463bf3419a80db60530f9
BasicCorruption <saytleader@outlook.com>
2022-06-26 18:42:49 -0400

    Delete ItemBase.java

--- src/main/java/dev/sidgames/wmo/item/ItemBase.java
+++
@@ -1,9 +0,0 @@
-package dev.sidgames.wmo.item;
-
-import net.minecraft.item.Item;
-
-public class ItemBase extends Item {
-    public ItemBase(Settings settings) {
-        super(settings);
-    }
-}
