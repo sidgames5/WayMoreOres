235db692eb4a580f781baa73f9ab82d6ba6ec416
BasicCorruption <saytleader@outlook.com>
2022-06-25 20:45:08 -0400

    Update WMOConfiguredFeatures.java

--- src/main/java/dev/sidgames/wmo/world/feature/WMOConfiguredFeatures.java
+++ src/main/java/dev/sidgames/wmo/world/feature/WMOConfiguredFeatures.java
@@ -23,12 +23,26 @@ public class WMOConfiguredFeatures {
                     BlockLoader.LITHIUM_ORE.getDefaultState()),
             OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
                     BlockLoader.DEEPSLATE_LITHIUM_ORE.getDefaultState()));
+    public static final List<OreFeatureConfig.Target> OVERWORLD_PLATINUM_ORES = List.of(
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
+                    BlockLoader.PLATINUM_ORE.getDefaultState()),
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                    BlockLoader.DEEPSLATE_PLATINUM_ORE.getDefaultState()));
+    public static final List<OreFeatureConfig.Target> OVERWORLD_RUBY_ORES = List.of(
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
+                    BlockLoader.RUBY_ORE.getDefaultState()),
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                    BlockLoader.DEEPSLATE_RUBY_ORE.getDefaultState()));
     public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> BAUXITE_ORE = ConfiguredFeatures.register("bauxite_ore", Feature.ORE,
             new OreFeatureConfig(OVERWORLD_BAUXITE_ORES, 9));
     public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> LEAD_ORE = ConfiguredFeatures.register("lead_ore", Feature.ORE,
             new OreFeatureConfig(OVERWORLD_LEAD_ORES, 9));
     public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> LITHIUM_ORE = ConfiguredFeatures.register("lithium_ore", Feature.ORE,
             new OreFeatureConfig(OVERWORLD_LITHIUM_ORES, 9));
+    public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> PLATINUM_ORE = ConfiguredFeatures.register("platinum_ore", Feature.ORE,
+            new OreFeatureConfig(OVERWORLD_PLATINUM_ORES, 9));
+    public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> RUBY_ORE = ConfiguredFeatures.register("ruby_ore", Feature.ORE,
+            new OreFeatureConfig(OVERWORLD_RUBY_ORES, 9));
 
     public static void registerConfiguredFeatures() {
         Reference.LOGGER.info("Registering configured features");
