47052c14e30ebb6baa2a638a35e3bf8d519e2362
BasicCorruption <saytleader@outlook.com>
2022-06-26 18:39:51 -0400

    Delete BlockBase.java

--- src/main/java/dev/sidgames/wmo/block/BlockBase.java
+++
@@ -1,9 +0,0 @@
-package dev.sidgames.wmo.block;
-
-import net.minecraft.block.Block;
-
-public class BlockBase extends Block {
-    public BlockBase(Settings settings) {
-        super(settings);
-    }
-}
