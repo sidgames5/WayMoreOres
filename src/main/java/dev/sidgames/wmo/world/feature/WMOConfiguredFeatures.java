e443f40f6e8522d78e8107a63e96984361002e84
BasicCorruption <saytleader@outlook.com>
2022-06-25 20:14:43 -0400

    Update WMOConfiguredFeatures.java

--- src/main/java/dev/sidgames/wmo/world/feature/WMOConfiguredFeatures.java
+++ src/main/java/dev/sidgames/wmo/world/feature/WMOConfiguredFeatures.java
@@ -18,10 +18,17 @@ public class WMOConfiguredFeatures {
                     BlockLoader.LEAD_ORE.getDefaultState()),
             OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
                     BlockLoader.DEEPSLATE_LEAD_ORE.getDefaultState()));
+    public static final List<OreFeatureConfig.Target> OVERWORLD_LITHIUM_ORES = List.of(
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
+                    BlockLoader.LITHIUM_ORE.getDefaultState()),
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                    BlockLoader.DEEPSLATE_LITHIUM_ORE.getDefaultState()));
     public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> BAUXITE_ORE = ConfiguredFeatures.register("bauxite_ore", Feature.ORE,
             new OreFeatureConfig(OVERWORLD_BAUXITE_ORES, 9));
     public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> LEAD_ORE = ConfiguredFeatures.register("lead_ore", Feature.ORE,
             new OreFeatureConfig(OVERWORLD_LEAD_ORES, 9));
+    public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> LITHIUM_ORE = ConfiguredFeatures.register("lithium_ore", Feature.ORE,
+            new OreFeatureConfig(OVERWORLD_LITHIUM_ORES, 9));
 
     public static void registerConfiguredFeatures() {
         Reference.LOGGER.info("Registering configured features");
