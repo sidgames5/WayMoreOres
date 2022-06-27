3fcf7a047823b38987cd2d21598d4e2e94ae26af
BasicCorruption <saytleader@outlook.com>
2022-06-26 21:03:35 -0400

    Create WMOPickaxeItem.java

---
+++ src/main/java/dev/sidgames/wmo/item/tool/WMOPickaxeItem.java
@@ -0,0 +1,10 @@
+package dev.sidgames.wmo.item.tool;
+
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class WMOPickaxeItem extends PickaxeItem {
+    public WMOPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
