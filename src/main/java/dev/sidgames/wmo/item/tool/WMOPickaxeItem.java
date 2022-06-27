1ae08d9486d059cdcc57f8f9e0f648930430e1e4
BasicCorruption <saytleader@outlook.com>
2022-06-27 09:38:57 -0400

    Update WMOPickaxeItem.java

--- src/main/java/dev/sidgames/wmo/item/tool/WMOPickaxeItem.java
+++ src/main/java/dev/sidgames/wmo/item/tool/WMOPickaxeItem.java
@@ -1,10 +1,12 @@
 package dev.sidgames.wmo.item.tool;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
 
 public class WMOPickaxeItem extends PickaxeItem {
-    public WMOPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
-        super(material, attackDamage, attackSpeed, settings);
+    public WMOPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed) {
+        super(material, attackDamage, attackSpeed, new Item.Settings().group(ItemGroup.TOOLS));
     }
 }
