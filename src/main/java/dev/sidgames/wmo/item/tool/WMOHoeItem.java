1cdafc114656934f1cbba2cc7d0f3e166ebe87b8
BasicCorruption <saytleader@outlook.com>
2022-06-26 21:03:33 -0400

    Create WMOHoeItem.java

---
+++ src/main/java/dev/sidgames/wmo/item/tool/WMOHoeItem.java
@@ -0,0 +1,10 @@
+package dev.sidgames.wmo.item.tool;
+
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class WMOHoeItem extends HoeItem {
+    public WMOHoeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
