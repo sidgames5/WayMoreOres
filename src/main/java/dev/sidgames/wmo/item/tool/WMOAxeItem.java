8bc45fdde012e4f0259625c476003c66b1c53fa0
BasicCorruption <saytleader@outlook.com>
2022-06-26 21:03:29 -0400

    Create WMOAxeItem.java

---
+++ src/main/java/dev/sidgames/wmo/item/tool/WMOAxeItem.java
@@ -0,0 +1,10 @@
+package dev.sidgames.wmo.item.tool;
+
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class WMOAxeItem extends AxeItem {
+    public WMOAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
