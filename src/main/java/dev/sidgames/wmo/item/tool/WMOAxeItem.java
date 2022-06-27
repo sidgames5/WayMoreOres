fedbbae5c22a417788cb77ec6b8b519bfc3d8348
BasicCorruption <saytleader@outlook.com>
2022-06-27 09:38:49 -0400

    Update WMOAxeItem.java

--- src/main/java/dev/sidgames/wmo/item/tool/WMOAxeItem.java
+++ src/main/java/dev/sidgames/wmo/item/tool/WMOAxeItem.java
@@ -1,10 +1,12 @@
 package dev.sidgames.wmo.item.tool;
 
 import net.minecraft.item.AxeItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ToolMaterial;
 
 public class WMOAxeItem extends AxeItem {
-    public WMOAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
-        super(material, attackDamage, attackSpeed, settings);
+    public WMOAxeItem(ToolMaterial material, float attackDamage, float attackSpeed) {
+        super(material, attackDamage, attackSpeed, new Item.Settings().group(ItemGroup.TOOLS));
     }
 }
