f608a782a2f569540a9235adcc032c420e1956fc
BasicCorruption <saytleader@outlook.com>
2022-06-27 22:23:28 -0400

    Update ReferenceConstants.java

--- src/main/java/dev/sidgames/wmo/ReferenceConstants.java
+++ src/main/java/dev/sidgames/wmo/ReferenceConstants.java
@@ -7,8 +7,11 @@ public class ReferenceConstants {
     public static final Logger LOGGER = LoggerFactory.getLogger(ReferenceConstants.MOD_ID);
     public static final String MOD_ID = "wmo";
     public static final String MOD_NAME = "Way More Ores";
-    public static final String MOD_VERSION = "1.0.0";
+    public static final String MOD_VERSION = "1.5.0";
+    public static final String MOD_VERSION_MODIFIER = "alpha.1";
     public static final String[] MOD_AUTHOR = {"SidGames5", "LeWolfYT"};
 
     public static final String MINECRAFT_VERSION = "1.18.2";
+    public static final String LOADER_VERSION = "0.14.8";
+    public static final String YARN_MAPPINGS = "1.18.2+build.3";
 }
