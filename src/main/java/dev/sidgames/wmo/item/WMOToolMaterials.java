4de73f904d6acafc9d05e57ebc790215188205d9
BasicCorruption <saytleader@outlook.com>
2022-06-29 16:26:43 -0400

    Update WMOToolMaterials.java

--- src/main/java/dev/sidgames/wmo/item/WMOToolMaterials.java
+++ src/main/java/dev/sidgames/wmo/item/WMOToolMaterials.java
@@ -8,15 +8,15 @@ import net.minecraft.util.Lazy;
 import java.util.function.Supplier;
 
 public enum WMOToolMaterials implements ToolMaterial {
-    ALUMINUM(MiningLevels.IRON, 90, 1.8F, 0.0F, 14, () -> Ingredient.ofItems(Items.ALUMINUM_INGOT)),
-    BRONZE(MiningLevels.IRON, 550, 11.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.BRONZE_ALLOY)),
-    LEAD(MiningLevels.IRON, 15, 0.3F, 0.0F, 14, () -> Ingredient.ofItems(Items.LEAD_INGOT)),
-    LITHIUM(MiningLevels.IRON, 20, 0.4F, 0.0F, 14, () -> Ingredient.ofItems(Items.LITHIUM_INGOT)),
-    PLATINUM(MiningLevels.IRON, 150, 3.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.PLATINUM_INGOT)),
-    RUBY(MiningLevels.IRON, 9, 0.2F, 0.0F, 14, () -> Ingredient.ofItems(Items.RUBY_GEM)),
-    SAPPHIRE(MiningLevels.IRON, 9, 0.2F, 0.0F, 14, () -> Ingredient.ofItems(Items.SAPPHIRE_GEM)),
-    SILVER(MiningLevels.IRON, 110, 2.2F, 0.0F, 14, () -> Ingredient.ofItems(Items.SILVER_INGOT)),
-    TIN(MiningLevels.IRON, 220, 4.4F, 0.0F, 14, () -> Ingredient.ofItems(Items.TIN_INGOT));
+    ALUMINUM(MiningLevels.IRON, 90, 3.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.ALUMINUM_INGOT)),
+    BRONZE(MiningLevels.IRON, 550, 6.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.BRONZE_ALLOY)),
+    LEAD(MiningLevels.IRON, 15, 4.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.LEAD_INGOT)),
+    LITHIUM(MiningLevels.IRON, 20, 5.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.LITHIUM_INGOT)),
+    PLATINUM(MiningLevels.IRON, 150, 6.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.PLATINUM_INGOT)),
+    RUBY(MiningLevels.IRON, 9, 3.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.RUBY_GEM)),
+    SAPPHIRE(MiningLevels.IRON, 9, 3.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.SAPPHIRE_GEM)),
+    SILVER(MiningLevels.IRON, 110, 3.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.SILVER_INGOT)),
+    TIN(MiningLevels.IRON, 220, 5.0F, 0.0F, 14, () -> Ingredient.ofItems(Items.TIN_INGOT));
 
     private final int miningLevel;
     private final int itemDurability;
