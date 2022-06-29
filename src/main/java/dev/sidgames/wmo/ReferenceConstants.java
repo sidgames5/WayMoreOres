457231dbcbc7ff0aa08b4593e3a6c6ef2a41aa9c
BasicCorruption <saytleader@outlook.com>
2022-06-29 15:32:46 -0400

    Update ReferenceConstants.java

--- src/main/java/dev/sidgames/wmo/ReferenceConstants.java
+++ src/main/java/dev/sidgames/wmo/ReferenceConstants.java
@@ -8,7 +8,7 @@ public class ReferenceConstants {
 
     public static final String MOD_ID = "wmo";
     public static final String MOD_NAME = "Way More Ores";
-    public static final String MOD_VERSION = "1.5.0-alpha.1";
+    public static final String MOD_VERSION = "1.5.0";
     public static final String[] MOD_AUTHOR = {"SidGames5", "LeWolfYT"};
 
     public static final String MINECRAFT_VERSION = "1.18.2";
