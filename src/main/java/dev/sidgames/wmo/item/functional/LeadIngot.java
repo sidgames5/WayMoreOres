0c145fb776ecbf4639def8c9b990951aae653ba3
BasicCorruption <saytleader@outlook.com>
2022-06-29 17:07:13 -0400

    Update LeadIngot.java

--- src/main/java/dev/sidgames/wmo/item/functional/LeadIngot.java
+++ src/main/java/dev/sidgames/wmo/item/functional/LeadIngot.java
@@ -2,15 +2,10 @@ package dev.sidgames.wmo.item.functional;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
