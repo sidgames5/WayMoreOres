54c2c012632f014262c96af7dbaa32da0c6a74bd
BasicCorruption <saytleader@outlook.com>
2022-07-02 16:26:12 -0400

    add the first item

--- src/main/java/dev/sidgames/wmo/WayMoreOres.java
+++ src/main/java/dev/sidgames/wmo/WayMoreOres.java
@@ -1,10 +1,12 @@
 package dev.sidgames.wmo;
 
 import com.mojang.logging.LogUtils;
+import dev.sidgames.wmo.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -28,7 +30,11 @@ public class WayMoreOres {
     public WayMoreOres()
     {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/dev/sidgames/wmo/item/ModItems.java
+++ src/main/java/dev/sidgames/wmo/item/ModItems.java
@@ -1,12 +1,19 @@
 package dev.sidgames.wmo.item;
 
 import dev.sidgames.wmo.WayMoreOres;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WayMoreOres.MOD_ID);
 
+    public static final RegistryObject<Item> ALUMINUM_INGOT = ITEMS.register("aluminum_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
