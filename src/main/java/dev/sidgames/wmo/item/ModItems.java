fb3474109226ac30719a998f1a551fff80ab7873
BasicCorruption <saytleader@outlook.com>
2022-07-02 16:16:28 -0400

    copy over the assets and data (same mc version)

---
+++ src/main/java/dev/sidgames/wmo/item/ModItems.java
@@ -0,0 +1,12 @@
+package dev.sidgames.wmo.item;
+
+import dev.sidgames.wmo.WayMoreOres;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WayMoreOres.MOD_ID);
+
+
+}
