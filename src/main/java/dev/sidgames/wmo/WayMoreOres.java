1bc142d1cd97043f010ac7a94e3805a65b314670
BasicCorruption <saytleader@outlook.com>
2022-07-02 22:19:32 -0400

    Update WayMoreOres.java

--- src/main/java/dev/sidgames/wmo/WayMoreOres.java
+++ src/main/java/dev/sidgames/wmo/WayMoreOres.java
@@ -1,6 +1,7 @@
 package dev.sidgames.wmo;
 
 import com.mojang.logging.LogUtils;
+import dev.sidgames.wmo.block.ModBlocks;
 import dev.sidgames.wmo.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -24,6 +25,7 @@ public class WayMoreOres {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
