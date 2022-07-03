315f4531f73bed7c8c99e1f946ba1221bee23a58
BasicCorruption <saytleader@outlook.com>
2022-07-03 15:34:56 -0400

    Create LeadIngot.java

---
+++ src/main/java/dev/sidgames/wmo/item/LeadIngot.java
@@ -0,0 +1,24 @@
+package dev.sidgames.wmo.item;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class LeadIngot extends Item {
+    public LeadIngot(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        // give the player poison for 20 seconds (400 ticks)
+        pPlayer.addEffect(new MobEffectInstance(MobEffects.POISON, 400, 1));
+        return super.use(pLevel, pPlayer, pUsedHand);
+    }
+}
