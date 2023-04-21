61a7da3c613984b014606d06f4c6e0c54ac50c40
BasicCorruption <saytleader@outlook.com>
2023-04-21 07:22:07 -0400

    Bump version number

--- src/main/java/dev/sidgames/wmo/Reference.java
+++ src/main/java/dev/sidgames/wmo/Reference.java
@@ -7,11 +7,4 @@ public class Reference {
     public static final Logger LOGGER = LoggerFactory.getLogger(Reference.MOD_ID);
 
     public static final String MOD_ID = "wmo";
-    public static final String MOD_NAME = "Way More Ores";
-    public static final String MOD_VERSION = "23w16a";
-    public static final String[] MOD_AUTHOR = {"SidGames5", "LeWolfYT"};
-
-    public static final String MINECRAFT_VERSION = "1.19.4";
-    public static final String LOADER_VERSION = "0.14.19";
-    public static final String YARN_MAPPINGS = "1.19.4+build.2";
 }
